18a876cfa477ec53b908f3cba6b86f46a9486f86
lanhaoxiang <lanhaoxiang@qq.com>
2018-02-07 23:45:28 +0800

    Initial Commit

---
+++ Graphene/Graphene.h
@@ -0,0 +1,19 @@
+//
+//  Graphene.h
+//  Graphene
+//
+//  Created by David Lan on 2018/2/7.
+//  Copyright © 2018年 GXChain. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Graphene.
+FOUNDATION_EXPORT double GrapheneVersionNumber;
+
+//! Project version string for Graphene.
+FOUNDATION_EXPORT const unsigned char GrapheneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Graphene/PublicHeader.h>
+
+
