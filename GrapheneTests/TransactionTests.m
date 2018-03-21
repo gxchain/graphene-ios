f2d89c07214e0df67c2228c6295733c1d6eab25f
lanhaoxiang <lanhaoxiang@qq.com>
2018-03-21 20:57:50 +0800

    Chinese supported in memo

--- GrapheneTests/TransactionTests.m
+++ GrapheneTests/TransactionTests.m
@@ -38,7 +38,7 @@
         transferOp.to=@"1.2.21";
         transferOp.amount=[[AssetAmount alloc] initWithAsset:@"1.3.1" amount:1000000];
         transferOp.fee=[[AssetAmount alloc] initWithAsset:@"1.3.1" amount:0];
-        transferOp.memo=[MemoData memoWithPrivate:@"5Ka9YjFQtfUUX2DdnqkaPWH1rVeSeby7Cj2VdjRt79S9kKLvXR7" public:@"GXC67KQNpkkLUzBgDUkWqEBtojwqPgL78QCmTRRZSLugzKEzW4rSm" message:@"123a"];
+        transferOp.memo=[MemoData memoWithPrivate:@"5Ka9YjFQtfUUX2DdnqkaPWH1rVeSeby7Cj2VdjRt79S9kKLvXR7" public:@"GXC67KQNpkkLUzBgDUkWqEBtojwqPgL78QCmTRRZSLugzKEzW4rSm" message:@"屌不屌，来自GXS Native的转账"];
         TransactionBuilder* tx = [[TransactionBuilder alloc] initWithOperations:@[transferOp]];
         [tx add_signer:[PrivateKey fromWif:@"5Ka9YjFQtfUUX2DdnqkaPWH1rVeSeby7Cj2VdjRt79S9kKLvXR7"]];
         [tx processTransaction:^(NSError* err,NSDictionary *transaction) {
